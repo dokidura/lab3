af1a684ae8618ad57648e86e741a1283fe6d2c5c
dokidura <glebossokin@gmail.com>
2023-10-25 00:55:32 +0300

    Revert "dsadasi"
    This reverts commit 562cd32f4a3363a0f1b328a54f89126c3e1a1341.

--- btn.js
+++
@@ -1,8 +0,0 @@
-const menuBTN = document.querySelector(".menuBTN")
-const after = document.querySelector(".after")
-
-function hide() {
-    after.classList.toggle("hidden");
-}
-
-menuBTN.addEventListener("click", hide)
